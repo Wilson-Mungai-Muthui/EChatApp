416a91d9f5fcd34b0c8cb069e001e6671014e8f9
Wilson <wilson.mungai.muthui@gmail.com>
2023-07-30 22:28:03 +0300

    Upload

--- chatapp.py
+++ chatapp.py
@@ -1,3 +1,10 @@
 import streamlit as st
+import openai
+import os
+from audio_recorder_streamlit import audio_recorder
+from streamlit_chat import message
+from streamlit.logger import get_logger
+from dotenv import load_dotenv
+from outputSide import generateAudio
 
 st.write("Hello World")
