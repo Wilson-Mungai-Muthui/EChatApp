f2ce314ff00e69f39cfeefea1a904fa83fb12f9b
Wilson <wilson.mungai.muthui@gmail.com>
2023-07-31 22:17:35 +0300

    Upload

--- outputSide4.py
+++ outputSide4.py
@@ -5,4 +5,3 @@ def stream_audio(audio_file_path):
         st.audio(audio_file, format='audio/mp3', start_time=0)  # Set start_time to 0 to play from the beginning
 
 audio_file_path = "output.mp3"  # Replace this with your audio file path
-
